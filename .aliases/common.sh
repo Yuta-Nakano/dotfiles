8b773ef7832b4c9a4d7964e2c20e0145d41375f1
Yuta Nakano <Yuta-Nakano@users.noreply.github.com>
2020-05-01 12:07:18 +0900

    Update common.sh

--- .aliases/common.sh
+++ .aliases/common.sh
@@ -13,7 +13,6 @@ alias   reload='exec $SHELL -l'
 function history-all { history -E 1 }
 
 # brew
-# alias brew="env PATH=${PATH/${HOME}\/\.pyenv\/shims:/} brew"
 function brew-remove {
   brew rm "$1"
   brew rm $(join <(brew leaves) <(brew deps "$1"))
@@ -21,6 +20,31 @@ function brew-remove {
 alias brew-rm='brew-remove'
 alias brew-up='brew update && brew upgrade'
 
+# tree
+function glt-install {
+  brew install "$1"
+  if [ ! `echo $(which $1) | grep $1` ]; then
+    sudo apt install "$1"
+  fi
+}
+function glt-change {
+  brew install "$1"
+  if [ `echo $(which $1) | grep $1` ]; then
+    sudo apt remove -y "$1"
+    sudo apt autoremove
+  fi
+}
+function glt-remove {
+  brew rm "$1"
+  brew rm $(join <(brew leaves) <(brew deps "$1"))
+  if [ `echo $(which $1) | grep $1` ]; then
+    sudo apt remove "$1"
+  fi
+}
+
 
 # gitignore.io
 function gignore { curl -sLw n https://www.gitignore.io/api/$@ ;}
+
+# docker-compose
+alias balse='docker-compose down --rmi all --volumes'
